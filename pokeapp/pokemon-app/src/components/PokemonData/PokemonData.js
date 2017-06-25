f515fa1b96a4efeaed35a5df3afd6b61a2a38c6a
Eli Keetch <elikeetch1996@gmail.com>
2017-06-25 13:19:27 -0600

    06-25-17 poke app complete

--- pokeapp/pokemon-app/src/components/PokemonData/PokemonData.js
+++ pokeapp/pokemon-app/src/components/PokemonData/PokemonData.js
@@ -23,15 +23,13 @@ class PokemonData extends Component {
             {this.props.selectedPokemon ? 
             <div>
                 <div className="pokedex-image">
-                <img className={this.props.showStats ? "img-size2" : "img-size"} src={this.props.selectedPokemon.sprites.front_shiny} />
-                </div>
-               
-                <div className="name-container" >
-               <h1 className="pokemon-title">{this.props.selectedPokemon.name}</h1>
-               </div>
+                <img className={this.props.showStats ? "img-size2 " : " img-size  "} src={this.props.selectedPokemon.sprites.front_shiny} />
+                <div className={this.props.showStats ? "stats-div2" : "stats-div"}>
+                    <div className="weight-height-spec">
                 <p className="pokemon-p-details"><span className="item-bold">Weight:</span> {this.props.selectedPokemon.weight} KG </p>
                 <p className="pokemon-p-details"><span className="item-bold">Height:</span> {this.props.selectedPokemon.height} M</p>
                 <p className="pokemon-p-details"><span className="item-bold">Species:</span> {this.props.selectedPokemon.species.name} </p>
+                </div>
                 <div className="p-and-ul-div"><p className="pokemon-p"><span className="item-bold">Abilities:</span> <ul className="pokemon-ul">{this.props.selectedPokemon.abilities.map((element, index)=> {
                     console.log("inside ability map",  element.ability.name)
                     return (
@@ -50,8 +48,18 @@ class PokemonData extends Component {
                     {/*<button className="set-pokemon"> Show location on map</button>*/}
 
             </div>
+                </div>
+                
+               
+                <div className="name-container" >
+               <h1 className="pokemon-title">{this.props.selectedPokemon.name}</h1>
+               </div>
+
+               
+            </div>
             : ""}
             </div>
+            
         );
     }
 }
